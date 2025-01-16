a3267ea7830688e21fbe3881e29fe56a48b3766c
DeKok, Matt G <matt.dekok@gmail.com>
2025-01-15 18:32:00 -0600

    Update D&D Beyond Tweaks to version 1.0.2, adding a new style for inspiration indicator

--- tampermonkey/ddbtweaks.user.js
+++ tampermonkey/ddbtweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         D&D Beyond Tweaks
 // @namespace    http://dndbeyond.com/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Adds extra tweaks for D&D Beyond
 // @supportURL   https://github.com/sillvva/sillvva.github.io/tree/main/tampermonkey
 // @downloadURL  https://sillvva.github.io/tampermonkey/ddbtweaks.user.js
@@ -16,3 +16,13 @@
 document.querySelectorAll(".magic-item-tooltip").forEach((mi) => {
   mi.after(" (magic item)");
 });
+
+if (inPages("/characters/")) {
+  addStyle(`
+    [data-testid="inspiration"][aria-checked="true"] {
+      -moz-box-shadow: 0px 0px 57px 3px rgba(255, 246, 145, 0.9), 0px 0px 13px -3px rgba(255, 247, 247, 0.9);
+      -webkit-box-shadow: 0px 0px 57px 3px rgba(255, 246, 145, 0.9), 0px 0px 13px -3px rgba(255, 247, 247, 0.9);
+      box-shadow: 0px 0px 57px 3px rgba(255, 246, 145, 0.9), 0px 0px 13px -3px rgba(255, 247, 247, 0.9);
+    }
+  `);
+}
